25c284b709727b5fb2bcd92d4100e894d1739d8a
Sidik Komarudiansah <sidikkomarudiansah@Sidiks-MacBook-Pro.local>
2024-06-09 01:40:19 +0700

    edit config cors 4

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -16,7 +16,7 @@ const login = async (req, res, next) => {
 
 				res.cookie('refreshToken', refreshToken, {
 					httpOnly: true,
-					secure: process.env.NODE_ENV === 'production',
+					secure: false,
 					sameSite: 'lax',
 					maxAge: 7 * 24 * 60 * 60 * 1000
 				});
