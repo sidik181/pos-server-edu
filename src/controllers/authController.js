5531a9666aa0c48835516878ca6c5294072ccfba
Sidik Komarudiansah <sidikkomarudiansah@Sidiks-MacBook-Pro.local>
2024-06-09 01:34:07 +0700

    edit config cors 3

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -17,7 +17,7 @@ const login = async (req, res, next) => {
 				res.cookie('refreshToken', refreshToken, {
 					httpOnly: true,
 					secure: process.env.NODE_ENV === 'production',
-					sameSite: process.env.NODE_ENV === 'production' ? 'strict' : 'lax',
+					sameSite: 'lax',
 					maxAge: 7 * 24 * 60 * 60 * 1000
 				});
 
