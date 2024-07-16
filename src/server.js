b1ff0eea3ae0ad248e1b3bd1d1dca8ac2d5aafbd
Sidik Komarudiansah <sidikkomarudiansah@Sidiks-MacBook-Pro.local>
2024-07-17 03:37:20 +0700

    fix bugs at file ser.js

--- src/server.js
+++ src/server.js
@@ -1,19 +1,20 @@
-import env from 'dotenv';
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-const app = express();
-env.config()
-import('./services/database.js');
+import dotenv from "dotenv";
+dotenv.config();
 
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import "./services/database.js";
+
+import authRouter from "./routes/authRoutes.js";
+import userRouter from "./routes/userRoutes.js";
+import productRouter from "./routes/productRoutes.js";
+import transactionRouter from "./routes/transactionRoutes.js";
+import { notFoundError, errorHandler } from "./middlewares/errorMiddleware.js";
+
+const app = express();
 const PORT = process.env.PORT || 3000;
 const FE_URI = process.env.FE_URI;
-
-import authRouter from './routes/authRoutes.js';
-import userRouter from './routes/userRoutes.js';
-import productRouter from './routes/productRoutes.js';
-import transactionRouter from './routes/transactionRoute.js';
-import { notFoundError, errorHandler } from './middlewares/errorMiddleware.js';
 
 const corsOptions = {
   origin: FE_URI,
@@ -25,13 +26,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use('/api', authRouter);
-app.use('/api', userRouter);
-app.use('/api', productRouter);
-app.use('/api', transactionRouter);
+app.use("/api", authRouter);
+app.use("/api", userRouter);
+app.use("/api", productRouter);
+app.use("/api", transactionRouter);
+
 app.use(notFoundError);
 app.use(errorHandler);
 
 app.listen(PORT, () => {
-	console.log(`Server is up on port ${PORT}.`)
+  console.log(`Server is up on port ${PORT}.`);
 });
